ed72948c897d254af12b8563c5dd4a32062d3fac
Aamir Iqbal <hafizaamir786@gmail.com>
2024-02-15 16:26:26 +0500

    responsive sell page

--- src/app/(default)/sell/page.tsx
+++ src/app/(default)/sell/page.tsx
@@ -5,14 +5,8 @@ import { Button } from '@components/ui/Button';
 
 function page() {
   return (
-    <div className="flex flex-col gap-10 bg-white">
-      <section
-        className="relative items-center justify-center"
-        style={{
-          height: 'calc(100vh - 70px)',
-          minHeight: '500px',
-        }}
-      >
+    <div className="flex flex-col gap-10 bg-white pb-10">
+      <section className="relative h-96 items-center justify-center lg:h-[calc(100vh-70px)]">
         <Image
           fill
           src="/images/webp/sell/mainEvalHeader.webp"
@@ -26,18 +20,18 @@ function page() {
 
         <div className="absolute  left-1/2 top-1/2 w-full max-w-4xl -translate-x-1/2 -translate-y-1/2">
           <div className="flex flex-col items-center gap-3">
-            <h3 className="w-[70%] text-center text-3xl font-medium text-white">
+            <h1 className="w-[70%] text-center text-xl font-medium text-white md:text-2xl lg:text-3xl">
               Sell Your Home with The Local Preserve Oakville Real Estate
               Experts
-            </h3>
+            </h1>
 
-            <div className="mx-auto mt-4 flex w-full max-w-lg items-center">
+            <div className="mx-auto mt-4 flex w-full max-w-xs items-center md:max-w-sm lg:max-w-lg">
               <Input
                 placeholder="Enter your address"
                 className="flex-1 rounded-r-none bg-white px-3"
               />
               <Button
-                className="w-1/4 rounded-l-none bg-primary-500"
+                className="rounded-l-none bg-primary-500 lg:w-1/4"
                 variant="default"
               >
                 Get Started
@@ -48,10 +42,10 @@ function page() {
       </section>
       <div className="container px-4">
         <div className="flex flex-col items-center gap-2">
-          <h4 className="w-[40%] text-center text-xl font-semibold 2xl:text-2xl">
+          <h2 className="text-center text-xl font-semibold md:w-[80%] lg:w-[40%] 2xl:text-2xl">
             We Know The Neighbourhood Best, when You Sell With Us You Get More!
-          </h4>
-          <p className="w-[80%] text-center text-sm xl:text-base">
+          </h2>
+          <p className="text-center text-sm md:w-[80%] xl:text-base">
             Enjoy a seamless and beneficial selling experience when working with
             us. Whether you&apos;re looking to sell your Condo, Townhouse,
             Semi-Detached or Detached home in The Preserve Oakville, we are here
@@ -64,25 +58,23 @@ function page() {
           </p>
         </div>
       </div>
-      <section className="relative overflow-hidden py-12 ">
-        <div className="absolute inset-0">
+      <section className="relative overflow-hidden py-12 lg:h-[50vh]">
+        <div className="absolute inset-0 hidden  md:block">
           <Image
-            src="/images/webp/sell/cta-banner.jpg"
+            src="/images/webp/sell/homeEvalBanner.webp"
             alt="banner"
             fill
-            className="h-full object-contain object-right"
+            className="object-cover"
           />
         </div>
 
-        <div className="absolute inset-0 hidden bg-gradient-to-r from-gray-100 via-transparent to-transparent md:block" />
-        {/* <div className="absolute inset-0 block bg-black/50 md:hidden" /> */}
-
-        <div className="container relative">
-          <div className="text-center md:w-2/3 md:text-left lg:w-1/2 xl:w-1/2">
-            <h2 className="font-semibold leading-tight text-black lg:text-xl 2xl:text-2xl">
+        <div className="absolute inset-0  bg-gradient-to-r from-gray-100 to-gray-100 md:block md:via-transparent md:to-transparent" />
+        <div className="container relative flex h-full items-center">
+          <div className="md:w-2/5 md:text-left lg:w-[42%]">
+            <h2 className="text-xl font-semibold 2xl:text-2xl">
               Hire A Real Estate Professional Who Never Gives Up
             </h2>
-            <p className="mt-4 text-sm text-gray-800 2xl:text-base">
+            <p className="mt-4 text-justify text-sm text-gray-800 2xl:text-base">
               Selling your home can be overwhelming, we help you simplify the
               process. Our dedicated team supports you from preparation to
               negotiations, ensures a successful closing and are there to answer
@@ -99,10 +91,10 @@ function page() {
           </div>
         </div>
       </section>
-      <h2 className="px-4 text-center text-2xl font-semibold">
+      <h2 className="px-4 text-center text-xl font-semibold 2xl:text-2xl">
         How Do We Help You ?
       </h2>
-      <div className="container grid grid-cols-2 items-center gap-20">
+      <div className="container grid items-center gap-10 md:grid-cols-2 lg:gap-20">
         <div className="relative mx-auto aspect-video w-[100%]">
           <Image
             src="/images/webp/sell/lawnSign.webp"
@@ -113,9 +105,9 @@ function page() {
         </div>
         <div className="flex flex-col gap-6">
           <div className="fle flex-col">
-            <h4 className="mb-1 text-lg font-medium 2xl:text-xl">
+            <h3 className="mb-1 text-lg font-medium 2xl:text-xl">
               Expert Guidance
-            </h4>
+            </h3>
             <p className="text-justify text-sm 2xl:text-base">
               Our seasoned agents bring a wealth of experience in selling a
               diverse range of properties, ensuring that you benefit from expert
@@ -127,9 +119,9 @@ function page() {
           </div>
 
           <div className="fle flex-col">
-            <h4 className="mb-1 text-lg font-medium 2xl:text-xl">
+            <h3 className="mb-1 text-lg font-medium 2xl:text-xl">
               Local Market Knowledge
-            </h4>
+            </h3>
             <p className="text-justify text-sm 2xl:text-base">
               Benefit from our in-depth knowledge of The Preserve&apos;s real
               estate market to highlight your property&apos;s unique strengths
@@ -142,12 +134,12 @@ function page() {
           </div>
         </div>
       </div>
-      <div className="container mb-10 grid grid-cols-2 items-center gap-20">
-        <div className="flex flex-col gap-6">
+      <div className="container  grid items-center gap-10 md:grid-cols-2 lg:gap-20">
+        <div className="order-2 flex flex-col gap-6 md:order-1">
           <div className="fle flex-col">
-            <h4 className="mb-1 text-lg font-medium 2xl:text-xl">
+            <h3 className="mb-1 text-lg font-medium 2xl:text-xl">
               Tailored Marketing Strategies
-            </h4>
+            </h3>
             <p className="text-justify text-sm 2xl:text-base">
               We craft personalized marketing strategies; focused on the
               distinct features of your property for maximum exposure. Through
@@ -158,9 +150,9 @@ function page() {
           </div>
 
           <div className="fle flex-col">
-            <h4 className="mb-1 text-lg font-medium 2xl:text-xl">
+            <h3 className="mb-1 text-lg font-medium 2xl:text-xl">
               Negotiation Excellence
-            </h4>
+            </h3>
             <p className="text-justify text-sm 2xl:text-base">
               Count on our team of skilled negotiators who work tirelessly to
               secure the best possible price and terms for your property. With a
@@ -172,7 +164,7 @@ function page() {
             </p>
           </div>
         </div>
-        <div className="relative mx-auto aspect-video w-[100%]">
+        <div className="relative order-1 mx-auto aspect-video w-[100%] md:order-2">
           <Image
             src="/images/webp/sell/3258Preserve.webp"
             alt="Tailored Marketing Strategies"
