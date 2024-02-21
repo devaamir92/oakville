fca5f7638112a64b756248bf657b82af6353b5b2
Aamir Iqbal <hafizaamir786@gmail.com>
2024-02-21 12:42:33 +0500

    updated button sizes on mobile for seo

--- src/components/ui/Search/index.tsx
+++ src/components/ui/Search/index.tsx
@@ -70,9 +70,10 @@ const SearchComponent: React.FC<Props> = ({ className, placeholder }) => {
     <DropdownMenu>
       <Root>
         <Trigger asChild ref={ref}>
-          {/* <label
+          <label
             htmlFor="search"
-            role="searchbox"
+            role="form"
+            aria-hidden="true"
             aria-label="Search for properties, agents, and more..."
             className={cn(
               'flex min-w-96 items-center gap-4 rounded px-2 text-sm',
@@ -88,25 +89,7 @@ const SearchComponent: React.FC<Props> = ({ className, placeholder }) => {
               onChange={handleSearch}
             />
             <FaSearch className="text-white" size={14} />
-          </label> */}
-          <div
-            role="searchbox"
-            aria-label="Search for properties, agents, and more..."
-            className={cn(
-              'flex min-w-96 items-center gap-4 rounded px-2 text-sm',
-              className
-            )}
-          >
-            <input
-              className="size-full bg-transparent outline-none placeholder:text-gray-100"
-              type="text"
-              id="search"
-              placeholder={placeholder}
-              value={searchTerm}
-              onChange={handleSearch}
-            />
-            <FaSearch className="text-white" size={14} />
-          </div>
+          </label>
         </Trigger>
         <Portal>
           <Content
