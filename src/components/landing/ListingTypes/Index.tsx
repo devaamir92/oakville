9cf37e8c36cab4e61572d36f5c6713e457b1ae7d
Aamir Iqbal <hafizaamir786@gmail.com>
2024-04-17 15:43:52 +0500

    set landing page links

--- src/components/landing/ListingTypes/Index.tsx
+++ src/components/landing/ListingTypes/Index.tsx
@@ -3,22 +3,32 @@ import Link from 'next/link';
 const Listings = [
   {
     id: 1,
+    title: 'New Listings',
+    href: '/homes-for-sale',
+  },
+  {
+    id: 2,
+    title: 'Sold',
+    href: '/sold-homes',
+  },
+  {
+    id: 3,
     title: 'Condos',
     href: '/condos-for-sale',
   },
   {
-    id: 2,
+    id: 4,
     title: 'Townhouses',
     href: '/condo-townhouses-for-sale',
   },
   {
-    id: 3,
+    id: 5,
     title: 'Detached',
     href: '/houses-for-sale',
   },
 
   {
-    id: 5,
+    id: 6,
     title: 'New Homes',
     href: '/new-homes',
   },
@@ -28,7 +38,7 @@ function ListingTypes() {
   return (
     <section className="bg-white">
       <div className="container">
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-6">
           {Listings.map(listing => (
             <Link
               href={listing.href}
