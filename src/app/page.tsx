b7a8e89fea86c9d31b53d2476352c24d4219eb4e
Aamir Iqbal <hafizaamir786@gmail.com>
2024-04-26 17:45:09 +0500

    set product details page jsonLD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import type { Metadata } from 'next';
 import type { SearchAction, WebSite, WithContext } from 'schema-dts';
 
+import Head from 'next/head';
+
 import { Desktop, Mobile } from '@components/ua';
 
 import Hero from '@components/landing/Hero';
@@ -19,6 +21,7 @@ import { getRecentlySold } from '@lib/api/properties/getRecentlySold';
 import { getDailyListing } from '@lib/api/properties/getDailyListing';
 import { getFeaturedListing } from '@lib/api/properties/getFeaturedListing';
 import Footer from '@components/Footer';
+import getSlug from '@utils/getSlug';
 
 type QueryAction = SearchAction & {
   'query-input': string;
@@ -86,6 +89,7 @@ const jsonLd: WithContext<WebSite> = {
   description:
     'Luxury real estate listings in Oakville, Ontario. Discover your dream home with The Preserve Oakville.',
 };
+
 const data = [
   {
     title: 'Schools For The Preserve Oakville',
@@ -131,14 +135,114 @@ const page = async () => {
     getRecentlySold(),
     getFeaturedListing(),
   ]);
+
   const session = await getSession();
+
   return (
     <>
-      <div className="flex flex-col gap-4 pb-4 md:gap-8 md:pb-8">
+      <Head>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {dailyListing?.data?.map((property: any) => (
+          <script
+            key={property.Ml_num}
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                '@context': 'https://schema.org',
+                '@type': 'Product',
+                name: `${property.Apt_num ? `${property.Apt_num} - ` : ''} ${
+                  property.Addr
+                } ${property.Ml_num}`,
+                description: `${
+                  property.Ad_text
+                    ? property.Ad_text.slice(0, 160)
+                    : `${property.Addr} is a beautiful luxury property in ${property.Community}. It contains ${property.Br} Bedrooms,${property.Bath_tot} bathrooms,${property.Tot_park_spcs} Parking.`
+                }`,
+                logo: 'https://preserveoakville.ca/images/png/preserveOakville.png',
+                brand: {
+                  '@type': 'Brand',
+                  name: 'Preserve Oakville',
+                },
+                category: property.Type_own1_out,
+                sku: property.Ml_num,
+                offers: {
+                  '@type': 'Offer',
+                  priceCurrency: 'CAD',
+                  price: property.Lp_dol,
+                  url: `https://preserveoakville.ca${getSlug(
+                    property.Community,
+                    property.Slug
+                  )}`,
+                  sku: property.Ml_num,
+                  businessFunction: property.S_r,
+                  availability:
+                    property.Status === 'U'
+                      ? 'https://schema.org/SoldOut'
+                      : 'https://schema.org/InStock',
+                },
+                url: `https://preserveoakville.ca${getSlug(
+                  property.Community,
+                  property.Slug
+                )}`,
+                image: `https://api.preserveoakville.ca/api/v1/stream/og/${property.Ml_num}/photo_1.png`,
+                // Add other property details as needed
+              }),
+            }}
+          />
+        ))}
+        {recentlySold?.data?.map((property: any) => (
+          <script
+            key={property.Ml_num}
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                '@context': 'https://schema.org',
+                '@type': 'Product',
+                name: `${property.Apt_num ? `${property.Apt_num} - ` : ''} ${
+                  property.Addr
+                } ${property.Ml_num}`,
+                description: `${
+                  property.Ad_text
+                    ? property.Ad_text.slice(0, 160)
+                    : `${property.Addr} is a beautiful luxury property in ${property.Community}. It contains ${property.Br} Bedrooms,${property.Bath_tot} bathrooms,${property.Tot_park_spcs} Parking.`
+                }`,
+                logo: 'https://preserveoakville.ca/images/png/preserveOakville.png',
+                brand: {
+                  '@type': 'Brand',
+                  name: 'Preserve Oakville',
+                },
+                category: property.Type_own1_out,
+                sku: property.Ml_num,
+                offers: {
+                  '@type': 'Offer',
+                  priceCurrency: 'CAD',
+                  price: property.Lp_dol,
+                  url: `https://preserveoakville.ca${getSlug(
+                    property.Community,
+                    property.Slug
+                  )}`,
+                  sku: property.Ml_num,
+                  businessFunction: property.S_r,
+                  availability:
+                    property.Status === 'U'
+                      ? 'https://schema.org/SoldOut'
+                      : 'https://schema.org/InStock',
+                },
+                url: `https://preserveoakville.ca${getSlug(
+                  property.Community,
+                  property.Slug
+                )}`,
+                image: `https://api.preserveoakville.ca/api/v1/stream/og/${property.Ml_num}/photo_1.png`,
+                // Add other property details as needed
+              }),
+            }}
+          />
+        ))}
+      </Head>
+      <div className="flex flex-col gap-4 pb-4 md:gap-8 md:pb-8">
         <Desktop>
           <Hero />
         </Desktop>
